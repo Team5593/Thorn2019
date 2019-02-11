ef5cb3c89ef1b5f3040249cd57a9b03656e3a659
Thomas <RoboStudent@SET-ENG-FMPY4S2.utas.ad.internal>
2019-02-11 15:46:36 +1100

    updated speed variable for hatch pitch

--- src/main/java/frc/robot/commands/PitchHatch.java
+++ src/main/java/frc/robot/commands/PitchHatch.java
@@ -5,14 +5,14 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class PitchHatch extends Command {
-  public PitchHatch(boolean isForwards, float speed) {
+  public PitchHatch(boolean isForwards, double speed) {
     this.isForwards = isForwards;
     this.speed = speed;
     requires(Robot.hatchPanelSubsystem);
   }
 
   private boolean isForwards;
-  private float speed;
+  private double speed;
 
   @Override
   protected void initialize() {
