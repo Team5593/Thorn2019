0d891911a9c50781b9eaab9b6eefd137bc1f3584
Liam Kinne <liamkinne@users.noreply.github.com>
2019-07-21 14:26:46 +1000

    Changed buttons to joysticks

--- src/main/java/frc/robot/OperatorInterface.java
+++ src/main/java/frc/robot/OperatorInterface.java
@@ -1,15 +1,14 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OperatorInterface {
   public XboxController gamepad = new XboxController(0);
-  public Button aButton = new JoystickButton(gamepad, 1);
-  public Button bButton = new JoystickButton(gamepad, 2);
-  public Button xButton = new JoystickButton(gamepad, 3);
-  public Button yButton = new JoystickButton(gamepad, 4);
-  public Button lbButton = new JoystickButton(gamepad, 5);
-  public Button rbButton = new JoystickButton(gamepad, 6);
+  public JoystickButton aButton = new JoystickButton(gamepad, 1);
+  public JoystickButton bButton = new JoystickButton(gamepad, 2);
+  public JoystickButton xButton = new JoystickButton(gamepad, 3);
+  public JoystickButton yButton = new JoystickButton(gamepad, 4);
+  public JoystickButton lbButton = new JoystickButton(gamepad, 5);
+  public JoystickButton rbButton = new JoystickButton(gamepad, 6);
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,12 +15,11 @@ public class Robot extends TimedRobot {
   public static DriveTrainSubsystem driveTrainSubsystem = new DriveTrainSubsystem();
   public static HatchPanelSubsystem hatchPanelSubsystem = new HatchPanelSubsystem();
   public static RampSubsystem rampSubsystem = new RampSubsystem();
-  public static OperatorInterface operatorInterface;
+  public static OperatorInterface operatorInterface = new OperatorInterface();
 
   @Override
   public void robotInit() {
     CameraServer.getInstance().startAutomaticCapture();
-    operatorInterface = new OperatorInterface();
     operatorInterface.aButton.whenPressed(new ReleaseHatchPanel(0.5));
     operatorInterface.lbButton.whileHeld(new PitchHatch(true, 0.5));
     operatorInterface.rbButton.whileHeld(new PitchHatch(false, 0.5));
